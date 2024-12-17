f62815cea61fdf6b1fea8f0442c5d6dfa676e8e9
muhammad fauzi masykur <muhammad.masykur@gojek.com>
2024-12-17 14:23:51 +0700

    fix: redirection to privacy policy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // This is fine since you're using a custom domain
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   }
 })
